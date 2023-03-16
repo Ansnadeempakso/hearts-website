0ae47f2822f9abf79a21e7e671fbe3ded582e768
Ansnadeempakso <128004410+Ansnadeempakso@users.noreply.github.com>
2023-03-16 05:43:48 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ function createHeart() {
     const heart = document.createElement('div');
     heart.classList.add('heart');
     heart.style.left = Math.random() * 100 + 'vw';
+    heart.style.top = Math.random() * 100 + 'vh';
     heart.style.animationDuration = Math.random() * 2 + 3 + 's';
 
-    document.querySelector('.hearts').appendChild(heart);
+    document.querySelector('body').appendChild(heart);
 
     setTimeout(() => {
         heart.remove();
